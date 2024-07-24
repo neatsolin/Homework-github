78b47039c07f09b815724128124baeb2e7786c39
unknown <Solin.Neat@PNC.LAN>
2024-07-24 17:33:45 +0700

    Home5

--- Practice3/work.py
+++
@@ -1,7 +0,0 @@
-string_indexing = str(input("enter a string:"))
-print(" first character:",string_indexing[0])
-print(" last character:",string_indexing[-1])
-print(" first character:",string_indexing[0:3])
-print(" last character:",string_indexing[-1])
-print("string reverse :",string_indexing[2])
-print(" every second character ",string_indexi[::2])
